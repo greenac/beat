beeaa33d44698dbeb7ee1c9bce4328d40307de3c
steven <sduval215@gmail.com>
2015-09-06 19:01:51 -0700

    angular templates/about stylings

---
+++ js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope){
+	$scope.info = 
+	{
+	title: "Increasing Science Literacy",
+	paraone: "What happens when you take college students and professors from different STEM disciplines and integrate them into local school systems? Better yet, what if they taught what they know best through fun, interactive, conceptual based modules? Natural science through the lens of real world examples. Technology through actual application.",
+	paratwo: "That's what we here at BEAT strive to do. We believe that STEM education is much more than textbooks and worksheets; it's everything and everywhere around us, and should be taught through experiments, interactive lectures and innovation. Through award winning modules and our expert content development team, we aim to provide elementary, middle and high school students the opportunity to see science and technology from a different perspective."
+	}
+}]);
